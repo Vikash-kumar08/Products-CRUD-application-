2ecb53c239b9d6110440ae0eb573b10ab3ac6c7c
Vikash-kumar08 <vikash.kumar@citymall.live>
2021-06-07 18:01:11 +0530

    finalcommit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AddProduct />
+    <Products />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Products from './components/Products.vue'
+import AddProduct from './components/AddProduct.vue'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Products,
+    AddProduct
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #2c3e50;
+  padding-top: 30px;
+}
+#app {
+  max-width: 700px;
+  margin: 0 auto;
 }
 </style>
